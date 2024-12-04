b482215188dd51d243b0fd6662b7bc8d92701a53
yakotdev <132854622+yakotdev@users.noreply.github.com>
2024-12-04 03:47:30 +0000

    فسش

--- medusa-config.js
+++ medusa-config.js
@@ -6,9 +6,9 @@ module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
+      storeCors: process.env.STORE_CORS || '',
+      adminCors: process.env.ADMIN_CORS || '',
+      authCors: process.env.AUTH_CORS || '',
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
